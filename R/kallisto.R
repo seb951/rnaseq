6e7df35ceaeae8b567c2a69cc0b1ce35f25a9787
EmilieBa <emilie.barrette@criucpq.ulaval.ca>
2023-05-19 16:30:53 -0400

    Modifications & adding Sleuth

--- R/kallisto.R
+++ R/kallisto.R
@@ -57,33 +57,42 @@ kallisto = function(R1_trim = sequences()[[3]][1],
 }
 
 #=====================
-#load TxDB from https://www.gencodegenes.org/human/
+#Data infos
 #=====================
-txdb = gencode_txdb("43", "hg19", chrs = paste0("chr", c(seq_len(22), "X", "Y", "M")))
-k = keys(txdb, keytype = "TXNAME")
-tx2gene = select(txdb, k, "GENEID", "TXNAME")
+#Text file of run infos
+sampleTable = read.delim("data/sampleTable.txt")
+rownames(sampleTable) = sampleTable$Name
 
+#=====================
+#load TxDB from gencode (Release 43 (GRCh38.p13))
+#=====================
+txdb_v31_hg19_chr1 = gencode_txdb("43", "hg19", chrs = "chr1") 
+#chrs = paste0("chr", c(seq_len(22), "X", "Y", "M"))
+k = keys(txdb_v31_hg19_chr1, keytype = "TXNAME")
+tx2gene = select(txdb_v31_hg19_chr1, k, "GENEID", "TXNAME")
+tx2gene$TXNAME = sapply(strsplit(tx2gene$TXNAME,".",fixed = T), `[`, 1)
 
 #=====================
 #Load expression data
 #=====================
-dir = ('data/')
+dir = "data/"
 files = file.path(paste0(dir, "kallisto"), list.files(paste0(dir, "kallisto")), "abundance.h5")
 names(files) = list.files(paste0(dir, "kallisto"))
 
 #import abundance.h5 files
 txi = tximport(files, type = "kallisto", tx2gene = tx2gene, 
-               txIn = TRUE, txOut = FALSE, countsFromAbundance = "no",  ignoreTxVersion = TRUE, ignoreAfterBar = TRUE)
+               txIn = TRUE, txOut = FALSE, countsFromAbundance = "lengthScaledTPM",  ignoreTxVersion = TRUE, ignoreAfterBar=TRUE)
 
 #=====================
 #Create DESeqDataSet object
 #=====================
-dds = DESeqDataSetFromTximport(txi, 
-                               colData = sampleData, ~ individual + paris_classification)
+dds = DESeq2::DESeqDataSetFromTximport(txi, colData = sampleTable)
+#ERROR if (all(assay(se) == 0)) {
+#stop("all samples have 0 counts for all genes. check the counting script.")
 
 #Differential expression analysis
 dds = DESeq(dds)
-res <- results(dds, name = "paris_classification_0.IIa_vs_normal", alpha = 0.05)
+res = results(dds, name = "Sain vs tumeur", alpha = 0.05)
 
 #=====================
 #Generating plots
@@ -91,12 +100,12 @@ res <- results(dds, name = "paris_classification_0.IIa_vs_normal", alpha = 0.05)
 #count data transformation with variance stabilizing transformations (vst)
 variance = vst(dds)
 # PCA_plot (Principal Componenet Analysis)
-pcaData = plotPCA(vsd, intgroup=c("individual","paris_classification"), 
+pcaData = plotPCA(vst, intgroup=c("Name","Sain vs tumeur"), 
                   returnData=TRUE)
 percentVar = round(100 * attr(pcaData, "percentVar"))
 
-png("DGE_PCA-vst.Salmon.png", width=7, height=7, units = "in", res = 300)
-ggplot(pcaData, aes(PC1, PC2, colour = paris_classification)) + 
+png("DGE_PCA-vst.kallisto.png", width=7, height=7, units = "in", res = 300)
+ggplot2::ggplot(pcaData, aes(PC1, PC2, colour = paris_classification)) + 
     geom_point(size = 2) +theme_bw() + scale_color_manual(values = c("blue", "red")) +
     geom_text_repel(aes(label = individual), nudge_x = -1, nudge_y = 0.2, size = 3) +
     ggtitle("Principal Component Analysis (PCA)", subtitle = "vst transformation") +
@@ -116,30 +125,73 @@ p = EnhancedVolcano(data.frame(res), lab = NA, x = 'log2FoldChange', y = 'padj',
                     legend=c('NS','Log2 FC','Adjusted p-value', 'Adjusted p-value & Log2 FC'),
                     legendPosition = 'bottom', legendLabSize = 14, legendIconSize = 5.0)
 
-png("DGE_VolcanoPlots.Salmon.png", width=7, height=7, units = "in", res = 300)
+png("DGE_VolcanoPlots.kallisto.png", width=7, height=7, units = "in", res = 300)
 print(p)
 dev.off()
 
+
+
+
+#==============================================
+# OTHER ALTERNATIVE
+#==============================================
+
+
+# Script from : https://pachterlab.github.io/sleuth_walkthroughs/trapnell/analysis.html
 #=====================
-#Exporting results
+#Path kallisto results
 #=====================
-annoData = "data/refanno/gencode.v33.annotation_genes.txt"
-annoData = data.frame(fread(annoData))
+dir = "/data/"
+sample_id <- dir(file.path("/data/kallisto"))
+kal_dirs <- file.path("/data/kallisto", sample_id)
+kal_dirs
 
-normCounts = as.data.frame(counts(dds, normalized = TRUE))
-baseMeans = as.data.frame(sapply( levels(dds$paris_classification), 
-                                  function(lvl) rowMeans( counts(dds, normalized = TRUE)[, dds$paris_classification == lvl, drop = FALSE] ) ))
+#=====================
+#SampleTable (infos samples assiciated with kallisto quant)
+#=====================
+s2c = read.table(file.path("/data/", "sampleTable.txt"), header = TRUE, stringsAsFactors=FALSE)
+s2c = dplyr::select(s2c, sample = Name, Type)
+s2c
 
-normData = merge(annoData, merge(baseMeans, normCounts, by.x = 'row.names', by.y = 'row.names'), by.x = 'GeneID', by.y = 'Row.names')
-normData = normData[order(normData$Chromosome, normData$Start, normData$End),]
+s2c = dplyr::mutate(s2c, path = kal_dirs)
 
-deData = data.frame(res[,c(1,2,5,6)])
-colnames(deData) = c("baseMean","log2fc","pvalue","padj")
-deData = merge(annoData, deData, by.x = 'GeneID', by.y = 'row.names')
-deData = deData[order(deData$Chromosome, deData$Start, deData$End),]
+#=====================
+#Initialize sleuth object
+#=====================
+so = sleuth_prep(s2c, extra_bootstrap_summary = TRUE)
 
-write.table(normData, file="DGE_DESeq2_Means_and_NormalisedCount.kallisto.txt", 
-            sep = "\t", quote = F, row.names = F, col.names = T)
+#=====================
+#Including gene names for transcript-level analysis
+#=====================
+#Collect gene names
+mart = biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",
+                        dataset = "hsapiens_gene_ensembl",
+                        host = 'ensembl.org')
+t2g = biomaRt::getBM(attributes = c("ensembl_transcript_id","ensembl_transcript_id_version", "ensembl_gene_id",
+                                    "ensembl_gene_id_version","external_gene_name","description",
+                                    "chromosome_name","start_position",
+                                    "end_position","strand",
+                                    "entrezgene_id"), mart = mart)
+t2g = dplyr::rename(t2g, target_id = ensembl_transcript_id,
+                    ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
 
-write.table(deData, file="DGE_DESeq2_DE_results.kallisto.txt", 
-            sep = "\t", quote = F, row.names = F, col.names = T)
+t2g = dplyr::select(t2g, c('target_id', 'ens_gene', 'ext_gene'))
+
+#Adding gene names to sleuth table
+so = sleuth_prep(s2c, target_mapping = t2g)
+
+#fitting measurement error models
+so = sleuth_fit(so, ~1, 'reduced')
+
+so = sleuth_lrt(so, 'reduced', 'reduced')
+
+#View results
+sleuth_live(so)
+
+#PCA plot
+plot_pca(so, color_by = 'condition')
+
+#Plot group density for count distribution for each sample
+plot_group_density(so, use_filtered = TRUE, units = "est_counts",
+                   trans = "log", grouping = setdiff(colnames(so$sample_to_covariates),
+                                                     "sample"), offset = 1)

547b034d1b463922c33a48d245596e75e146bb6d
EmilieBa <emilie.barrette@criucpq.ulaval.ca>
2023-05-23 10:38:43 -0400

    Review

--- R/kallisto.R
+++ R/kallisto.R
@@ -1,12 +1,7 @@
-source('R/counts_functions.R')
 #kallisto function
-library(data.table)
-library(GenomicFeatures)
-library(tximport)
-library(ggplot2)
-library(ggrepel)
-library(rhdf5)
-library(GenomicState)
+source('R/counts_functions.R')
+#library(rhdf5) use to store large dataset???
+
 #=====================
 ###get sequences ready
 #=====================
@@ -36,9 +31,7 @@ kallisto = function(R1_trim = sequences()[[3]][1],
                    out_prefix = paste0('rnaseq/out/',sequences()[[5]][1],'_')
                    ) {
     
-    #index cmd
-    # index downloaded from here: https://www.gencodegenes.org/human/
-    #Crééer fichier contenant l'index
+    #index cmd, index downloaded from here: https://www.gencodegenes.org/human/
     index = paste0(ifelse(Sys.info()['sysname'] == 'Windows','wsl.exe ',''),'kallisto index -i ', in.dir, 'gencode.v43.transcripts.idx ', in.dir, 'gencode.v43.transcripts.fa.gz')
     system(index)
     
@@ -57,7 +50,6 @@ kallisto = function(R1_trim = sequences()[[3]][1],
     system(cmd)
     
     # message(cmd)
-    
     message(paste0('Done Kallisto, Time is: ',Sys.time()))
     
     return('')
@@ -66,28 +58,28 @@ kallisto = function(R1_trim = sequences()[[3]][1],
 #=====================
 #Data infos
 #=====================
-#Text file of run infos
+#Text file of run info
 sampleTable = read.delim("data/sampleTable.txt")
 rownames(sampleTable) = sampleTable$Name
 
 #=====================
 #load TxDB from gencode (Release 43 (GRCh38.p13))
 #=====================
-txdb_v31_hg19_chr1 = gencode_txdb("43", "hg19", chrs = "chr1") 
+txdb_v31_hg19_chr1 = GenomicState::gencode_txdb("43", "hg19", chrs = "chr1") 
 #chrs = paste0("chr", c(seq_len(22), "X", "Y", "M"))
 k = keys(txdb_v31_hg19_chr1, keytype = "TXNAME")
 tx2gene = select(txdb_v31_hg19_chr1, k, "GENEID", "TXNAME")
 tx2gene$TXNAME = sapply(strsplit(tx2gene$TXNAME,".",fixed = T), `[`, 1)
 
 #=====================
-#Load expression data
+#Load kallisto data
 #=====================
 dir = "data/"
 files = file.path(paste0(dir, "kallisto"), list.files(paste0(dir, "kallisto")), "abundance.h5")
 names(files) = list.files(paste0(dir, "kallisto"))
 
 #import abundance.h5 files
-txi = tximport(files, type = "kallisto", tx2gene = tx2gene, 
+txi = tximport::tximport(files, type = "kallisto", tx2gene = tx2gene, 
                txIn = TRUE, txOut = FALSE, countsFromAbundance = "lengthScaledTPM",  ignoreTxVersion = TRUE, ignoreAfterBar=TRUE)
 
 
@@ -100,7 +92,7 @@ sample_id = dir(file.path("/data/kallisto"))
 kal_dirs = file.path("/data/kallisto", sample_id)
 kal_dirs
 
-#SampleTable (infos samples associated with kallisto quant)
+#SampleTable (info of samples associated with kallisto quant)
 s2c = read.table(file.path("/data/", "sampleTable.txt"), header = TRUE, stringsAsFactors=FALSE)
 s2c = dplyr::select(s2c, sample = Name, Type)
 s2c = dplyr::mutate(s2c, path = kal_dirs)
@@ -109,15 +101,16 @@ s2c = dplyr::mutate(s2c, path = kal_dirs)
 so = sleuth_prep(s2c, extra_bootstrap_summary = TRUE)
 
 #Including gene names for transcript-level analysis
-#Collect gene names
 mart = biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL",
                         dataset = "hsapiens_gene_ensembl",
                         host = 'ensembl.org')
+
 t2g = biomaRt::getBM(attributes = c("ensembl_transcript_id","ensembl_transcript_id_version", "ensembl_gene_id",
                                     "ensembl_gene_id_version","external_gene_name","description",
                                     "chromosome_name","start_position",
                                     "end_position","strand",
                                     "entrezgene_id"), mart = mart)
+
 t2g = dplyr::rename(t2g, target_id = ensembl_transcript_id,
                     ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
 
@@ -128,16 +121,14 @@ so = sleuth_prep(s2c, target_mapping = t2g)
 
 #fitting measurement error models
 so = sleuth_fit(so, ~1, 'reduced')
-
 so = sleuth_lrt(so, 'reduced', 'reduced')
 
-#View results
-sleuth_live(so)
-
 #PCA plot
-plot_pca(so, color_by = 'condition')
+plot_pca(so, color_by = 'Type')
 
-#Plot group density for count distribution for each sample
-plot_group_density(so, use_filtered = TRUE, units = "est_counts",
+#Group density plot for count distribution/sample type
+plot_group_density(so, use_filtered = TRUE, units = "tpm",
                    trans = "log", grouping = setdiff(colnames(so$sample_to_covariates),
                                                      "sample"), offset = 1)
+
+#View results with sleuth_live(so)

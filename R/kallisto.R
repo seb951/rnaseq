1a5a3485ab79d3c83644c7aae117313d88e98dbe
EmilieBa <emilie.barrette@criucpq.ulaval.ca>
2023-05-29 16:21:57 -0400

    Correction

--- R/kallisto.R
+++ R/kallisto.R
@@ -5,45 +5,42 @@ source('R/counts_functions.R')
 #=====================
 #Pseudo-mapping step
 #=====================
-kallisto = function(fastq.dir = file.path('out/fastq.trim/'),
-                    out_prefix = 'out/kallisto/toto_',
-                    R1_trim = sequences()[[3]][1],
-                    R2_trim = sequences()[[4]][1],
-                    in.dir = file.path('data/index/'),
-                    out.dir = file.path('out/kallisto'),
-                    i = 1)
-    {
-    
+index = function(in.dir = file.path('data/index/'))
+{
     #index cmd, index downloaded from here: https://www.gencodegenes.org/human/ (gencode.v43.transcripts.idx)
-    index = paste0(ifelse(Sys.info()['sysname'] == 'Windows','wsl.exe ',''),'kallisto index -i ', in.dir, 'chr1.idx ', ' --make-unique ', in.dir, 'chr1.fa.gz')
+    index = paste0(ifelse(Sys.info()['sysname'] == 'Windows','wsl.exe ',''),'kallisto index -i ', in.dir, 'chr1.idx', ' --make-unique ', in.dir, 'chr1.fa.gz')
     system(index)
     
     # message(cmd)
     message(paste0('Done index, Time is: ',Sys.time()))
     
-    #files
-    files = list.files(path = "out/fastq.trim/", pattern = ".fq.gz")
-    
-    for(i in files)
-        {
-        out_prefix_nopath = paste0('out/kallisto/',sequences()[[5]][1])
-        
-        #kallisto cmd
-        cmd = paste0(ifelse(Sys.info()['sysname'] == 'Windows','wsl.exe ',''),'kallisto quant -i ', in.dir, 'chr1.idx -o ',
-                     out_prefix_nopath,
-                     ' -b 100 ', 
+}
+
+
+kallisto = function(fastq.dir = file.path('out/fastq.trim/'),
+                    in.dir = file.path('data/index/'),
+                    trim1 = list.files(path = "out/fastq.trim/", pattern = "val_1.fq.gz"),
+                    trim2 = list.files(path = "out/fastq.trim/", pattern = "val_2.fq.gz"),
+                    out.dir = file.path('out/kallisto/'),
+                    i = 1)
+    {
+    #kallisto cmd
+    cmd = paste0(ifelse(Sys.info()['sysname'] == 'Windows','wsl.exe ',''),'kallisto quant -i ', in.dir, 'chr1.idx ',
+                     '-o ',
+                     out.dir,
+                     ' -b 100 ',
                      fastq.dir,
-                     R1_trim,
+                     trim1,
                      ' ',
                      fastq.dir,
-                     R2_trim,
+                     trim2,
                      ' ', '1>', ifelse(i>1, '>', ''), file.path('out/logs'), '/kallisto.out 2>', ifelse(i>1, '>', ''), file.path('out/logs'), '/kallisto.err')
         
         system(cmd)
         
         # message(cmd)
         message(paste0('Done Kallisto, Time is: ',Sys.time()))
-        }
+
     }
 
 
@@ -57,18 +54,33 @@ kallisto_rnaseq = function(fastq.dir = params$fastq.dir,
                            out_prefix = params$out_prefix,
                            in.dir = params$in.dir,
                            out.dir = params$out.dir)
-{  
+{
+    index(in.dir = file.path('data/index/'))
+    
+    files = list.files(path = "out/fastq.trim/", pattern = ".fq.gz")
+    trim1 = list.files(path = "out/fastq.trim/", pattern = "val_1.fq.gz")
+    trim2 = list.files(path = "out/fastq.trim/", pattern = "val_2.fq.gz")
+    
+    name_samples = list(unique(substr(files, 1, 20)))
+    
+    for(i in files)
+    {
+        #output directory
+        path = 'out/kallisto/'
+        lapply(name_samples, dir.create(paste0(path, x, name_samples)))
+                       
+        #out_prefix = dir.create(paste0(path, name_samples[[c(i)]]))
         
         #kallisto
         kallisto(fastq.dir = file.path('out/fastq.trim/'),
-                 out_prefix = 'out/kallisto/toto_',
-                 R1_trim = sequences()[[3]][1],
-                 R2_trim = sequences()[[4]][1],
                  in.dir = file.path('data/index/'),
-                 out.dir = file.path('out/kallisto'),
+                 trim1 = list.files(path = "out/fastq.trim/", pattern = "val_1.fq.gz"),
+                 trim2 = list.files(path = "out/fastq.trim/", pattern = "val_2.fq.gz"),
+                 out.dir = out_prefix,
                  i = 1)
         
         #message
         message(paste0('--- Done sample, Time is: ',Sys.time(),' ---'))
+    }
     
 }

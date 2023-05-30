eb437a9875d0796238857b74b1f91df4b5b27402
EmilieBa <emilie.barrette@criucpq.ulaval.ca>
2023-05-30 11:33:38 -0400

    Corrected version

--- R/kallisto.R
+++ R/kallisto.R
@@ -57,31 +57,30 @@ kallisto_rnaseq = function(fastq.dir = params$fastq.dir,
 {
     index(in.dir = file.path('data/index/'))
     
-    files = list.files(path = "out/fastq.trim/", pattern = ".fq.gz")
     trim1 = list.files(path = "out/fastq.trim/", pattern = "val_1.fq.gz")
     trim2 = list.files(path = "out/fastq.trim/", pattern = "val_2.fq.gz")
     
-    name_samples = unique(substr(files, 1, 20))
+    name_samples = sapply(strsplit(trim1,"_val_"), `[`, 1)
     
-    
-    #output directory
-    path = 'out/kallisto/'
-    out_prefix = ifelse(!dir.exists(file.path(path, paste0(name_samples[1]))), dir.create(file.path(path, paste0(name_samples[1]))), FALSE)
-    #out_prefix = dir.create(path, paste0(name_samples[1]))
-    
-    for(i in files)
+    for(i in seq_along(name_samples)) 
     {
+        #create outpour
+        out_prefix = "out/kallisto"
+        dir.create(file.path(out_prefix, name_samples[i]), recursive = TRUE)
+        
         #kallisto
         kallisto(fastq.dir = file.path('out/fastq.trim/'),
                  in.dir = file.path('data/index/'),
-                 trim1 = list.files(path = "out/fastq.trim/", pattern = "val_1.fq.gz"),
-                 trim2 = list.files(path = "out/fastq.trim/", pattern = "val_2.fq.gz"),
-                 out.dir = out_prefix,
+                 trim1 = trim1[i],
+                 trim2 = trim2[i],
+                 out.dir = file.path(out_prefix, name_samples[i]),
                  i = 1)
+        #zip files
+        #files_zip = file.path(out_prefix, name_samples[i])
+        #zip(zipfile = files_zip, files = files_zip)
         
         #message
         message(paste0('--- Done sample, Time is: ',Sys.time(),' ---'))
     }
     
 }
-
